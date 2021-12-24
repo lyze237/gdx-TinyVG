23e1b3da60efa7a94a47e6414327a24cc9e004a1
Lyze <11274700+lyze237@users.noreply.github.com>
2021-12-24 14:36:20 +0100

    ✅ Fixed test

--- src/test/java/dev/lyze/gdxtinyvg/headless/tests/HeaderTest.java
+++ src/test/java/dev/lyze/gdxtinyvg/headless/tests/HeaderTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
 public class HeaderTest extends LibgdxHeadlessUnitTest {
     @Test
     public void SquareHeaderTest() {
-        var tvg = new TinyVGAssetLoader().load("twoSquares.tvg");
+        var tvg = new TinyVGAssetLoader().load("square.tvg");
 
         Assertions.assertEquals(ColorEncoding.RGBA8888, tvg.getHeader().getColorEncoding());
 
