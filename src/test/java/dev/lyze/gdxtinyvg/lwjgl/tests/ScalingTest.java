fee5e375b1a09e62ea31b30e88a968a6b292b9d6
Lyze <11274700+lyze237@users.noreply.github.com>
2022-02-08 00:54:20 +0100

    💥 Scaling is based on origin now.

---
+++ src/test/java/dev/lyze/gdxtinyvg/lwjgl/tests/ScalingTest.java
@@ -0,0 +1,79 @@
+package dev.lyze.gdxtinyvg.lwjgl.tests;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.profiling.GLProfiler;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import dev.lyze.gdxtinyvg.TinyVG;
+import dev.lyze.gdxtinyvg.TinyVGAssetLoader;
+import dev.lyze.gdxtinyvg.drawers.TinyVGShapeDrawer;
+import dev.lyze.gdxtinyvg.lwjgl.LibgdxLwjglUnitTest;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class ScalingTest extends LibgdxLwjglUnitTest {
+    private TinyVG tvg;
+    private TinyVGShapeDrawer drawer;
+    private Viewport viewport;
+
+    private GLProfiler profiler;
+
+    @Override
+    public void create() {
+        drawer = new TinyVGShapeDrawer(new SpriteBatch());
+        viewport = new FitViewport(100, 100);
+
+        profiler = new GLProfiler(Gdx.graphics);
+        profiler.enable();
+    }
+
+    @Test
+    @Tag("lwjgl")
+    public void pirate() {
+        setupTvg("pirate.tvg");
+    }
+
+    private void setupTvg(String file) {
+        Gdx.app.postRunnable(() -> {
+            tvg = new TinyVGAssetLoader().load(file);
+
+            viewport = new FitViewport(tvg.getScaledWidth() * 5, tvg.getScaledHeight() * 5);
+            tvg.centerOrigin();
+            tvg.setOriginBasedPosition(viewport.getWorldWidth() / 2f, viewport.getWorldHeight() / 2f);
+            resize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        });
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(.25f, .25f, .25f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_STENCIL_BUFFER_BIT);
+
+        if (tvg == null)
+            return;
+
+        viewport.apply();
+
+        drawer.getBatch().setProjectionMatrix(viewport.getCamera().combined);
+
+        drawer.getBatch().begin();
+        drawer.filledRectangle(0, 0, viewport.getWorldWidth(), viewport.getWorldHeight(), Color.TEAL);
+
+        tvg.setScale(tvg.getScaleX() + 0.5f * Gdx.graphics.getDeltaTime());
+        tvg.setRotation(tvg.getRotation() + 50 * Gdx.graphics.getDeltaTime());
+
+        profiler.reset();
+        tvg.draw(drawer);
+        System.out.println(profiler.getDrawCalls());
+
+        drawer.getBatch().end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
+    }
+}
